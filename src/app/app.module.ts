c088fc818cd92675337a853150b7349a78b81a08
REYE237 <lily.reyes@arvato-scs.com>
2021-07-11 12:42:24 +0200

    Components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingListEditComponent } from './components/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RecipeListComponent } from './components/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    HeaderComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from "@angular/core";
+import { ShoppingListEditComponent } from "./shopping-list-edit/shopping-list-edit.component";
+
+
+@Component({
+  selector:'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls:['./shopping-list.component.css']
+})
+export class ShoppingListComponent{
+
+}
