ce9928ae70205e37e4e98ada2c424ac8a39fa5d6
REYE237 <lily.reyes@arvato-scs.com>
2021-07-16 17:38:01 +0200

    Added navigation for Recipes & Shopping list in header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipes';
+  loadedFeature ='recipes';
+
+  onNavigation(feature:string){
+      this.loadedFeature=feature;
+
+  }
+
+
 }
